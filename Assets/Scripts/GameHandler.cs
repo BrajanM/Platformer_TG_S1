2683c99d95b1471541d9ef314b4d3f247b51b9d6
SandraW96 <s.wieczorek199@gmail.com>
2021-05-06 23:01:30 +0200

    Initial Commit

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour
+{
+	public GameObject RestartMenuPanel;
+	public GameObject Player;
+    
+	public static float GameSpeed;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		RestartMenuPanel.gameObject.SetActive(false);
+		GameSpeed = 4;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		//if (gameOver)
+		//{
+		//	RestartMenuPanel.gameObject.SetActive(true);
+		//}
+    }
+
+	//public void RestartButtonClick()
+	//{
+	//	SceneManager.LoadScene("SampleScene");
+	//}
+}
